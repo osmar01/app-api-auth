5c65a9368aebb55516f5cfbed43405f4d41d4717
osmar01 <osmarj.frota@gmail.com>
2024-09-19 23:12:29 -0400

    first commit

---
+++ src/main/java/com/jfsoftware/app_api_auth/AppApiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.jfsoftware.app_api_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiAuthApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jfsoftware/app_api_auth/domain/User.java
@@ -0,0 +1,23 @@
+package com.jfsoftware.app_api_auth.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
